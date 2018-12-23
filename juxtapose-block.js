6c1329fb86c2052fd25d2373590cc53d5f6922d6
Marcus Kazmierczak <marcus@mkaz.com>
2018-12-22 20:23:04 -0800

    Remove change image option, simplify interaction

--- juxtapose-block.js
+++ juxtapose-block.js
@@ -5,10 +5,9 @@
  * https://github.com/NUKnightLab/juxtapose
  */
 
-/**
- * WordPress dependencies
- */
 ( function(wp) {
+
+	// WordPress dependencies
 	const  el = wp.element.createElement;
     const registerBlockType = wp.blocks.registerBlockType;
 	const { InspectorControls, MediaUpload } = wp.editor;
@@ -41,65 +40,68 @@
 			let previewJuxtapose = null
 			let imgLeft = null;
 			let imgRight = null;
-			let actionLeftLabel = 'Add';
-			let actionRightLabel = 'Add';
+			let controlsLeft = null;
+			let controlsRight = null;
 
-			// mediaUploadControls
+			// if either attribute is defined, show image(s) selected
 			if ( attributes.imageLeft || attributes.imageRight ) {
 				if ( attributes.imageLeft ) {
 					imgLeft = el( 'img', { src: attributes.imageLeft } );
-					actionLeftLabel = 'Change';
 				}
 				if ( attributes.imageRight ) {
 					imgRight = el( 'img', { src: attributes.imageRight } );
-					actionRightLabel = 'Change';
 				}
 
+				// if both attributes defined, add juxtapose class which triggers view
 				cls = ( attributes.imageLeft && attributes.imageRight ) ? 'juxtapose' : '';
 				previewJuxtapose = el( 'div', { className: cls }, [ imgLeft, imgRight ]);
 			}
 
+			// show media upload button if left image not defined
+			if ( ! attributes.imageLeft ) {
+				controlsLeft = el( MediaUpload, {
+					onSelect: function(el) { setAttributes( { imageLeft: el.url } ); juxtapose.scanPage(); },
+					allowedTypes: [ 'image' ],
+					render: ( { open } ) => (
+						el( IconButton, {
+								icon: 'edit',
+								onClick: open,
+							},
+							'Add Left Image'
+						)
+					)
+				});
+			}
+
+			// show media upload button if right image not defined
+			if ( ! attributes.imageRight ) {
+				controlsRight = el( MediaUpload, {
+					onSelect: function(el) { setAttributes( { imageRight: el.url } ); juxtapose.scanPage(); },
+					allowedTypes: [ 'image' ],
+					render: ( { open } ) => (
+						el( IconButton, {
+								icon: 'edit',
+								onClick: open,
+							},
+							'Add Right Image'
+						)
+					)
+				});
+			}
+
 			const editControls = el( 'div', { className: 'edit-controls' }, [
-				el( 'div', { className: 'img-edit-left' },
-					el( MediaUpload, {
-						onSelect: function(el) { setAttributes( { imageLeft: el.url } ); juxtapose.scanPage(); },
-						allowedTypes: [ 'image' ],
-						render: ( { open } ) => (
-							el( IconButton, {
-									icon: 'edit',
-									onClick: open,
-								},
-								`${actionLeftLabel} Left Image`
-							)
-						)
-					})
-				),
-				el( 'div', { className: 'img-edit-right' },
-					el( MediaUpload, {
-						onSelect: function(el) { setAttributes( { imageRight: el.url } ); juxtapose.scanPage(); },
-						allowedTypes: [ 'image' ],
-						render: ( { open } ) => (
-							el( IconButton, {
-									icon: 'edit',
-									onClick: open,
-								},
-								`${actionRightLabel} Right Image`
-							)
-						)
-					})
-				)
+				el( 'div', { className: 'img-edit-left' }, controlsLeft ),
+				el( 'div', { className: 'img-edit-right' }, controlsRight )
 			]);
 
 			return [
 				el( InspectorControls, { key: 'controls' },
 					el( 'div', {}, 'Start Position' )
 				),
-				el( 'div', { className: className },
-					[
+				el( 'div', { className: className }, [
 						previewJuxtapose,
 						editControls
-					]
-				)
+				])
 			];
 		},
 
